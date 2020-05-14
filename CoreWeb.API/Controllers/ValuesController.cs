fcd161fae08aa1752d357c532439c2451d961d85
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-14 14:15:36 +0200

    corretto namespace controller Values

--- CoreWeb.API/Controllers/ValuesController.cs
+++ CoreWeb.API/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DatingApp.API.Controllers
+namespace CoreWeb.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
