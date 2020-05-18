fa92a4f1a7cb4c10f1b64108cab4c0ba3e298c7a
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-18 17:58:02 +0200

    generata migration

--- CoreWeb.API/Migrations/20200518153322_AddedUserEntity.Designer.cs
+++ CoreWeb.API/Migrations/20200518153322_AddedUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CoreWeb.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreWeb.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200518153322_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
