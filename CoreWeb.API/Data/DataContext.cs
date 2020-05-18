58a1922f9b18c8551aeb5fbfdb7ae0984da0cc15
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-18 10:56:21 +0200

    Aggiunta classe DataContext che rappresenta i contesti dati per EF. Aggiunto il primo contesto che è la tabella Values che è un DbSet della classe Value

---
+++ CoreWeb.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using CoreWeb.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWeb.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
