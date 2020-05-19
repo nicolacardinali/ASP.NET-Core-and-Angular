a13eafb5876b94ea4ae1b2723a29601093cfae97
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-19 16:34:05 +0200

    specifica dello schema di autenticazione

--- CoreWeb.API/Startup.cs
+++ CoreWeb.API/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CoreWeb.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoreWeb.API
 {
@@ -31,6 +34,18 @@ namespace CoreWeb.API
             services.AddControllers();
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       // Specifica quali sono i parametri di validazione del token
+                       ValidateIssuerSigningKey = true, // controlla che la chiave sia valida
+                       // Imposta la chiave che deve usare per validare il token
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)), 
+                       ValidateIssuer = false, // temporaneamente false perchè issuer e audience sono localhost
+                       ValidateAudience = false
+                   });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
