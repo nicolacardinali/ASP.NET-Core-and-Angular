a37f5015fc46de01a058f2289c58d5e8035310b2
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-20 08:54:27 +0200

    creato oggetto model e metodo login

--- CoreWeb-SPA/src/app/nav/nav.component.ts
+++ CoreWeb-SPA/src/app/nav/nav.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    console.log(this.model);
   }
 
 }
