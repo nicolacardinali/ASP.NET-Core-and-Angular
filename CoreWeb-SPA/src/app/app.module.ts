1053c82cec1b150b82be0264abee24fc29ae407d
Nicola Cardinali <ncardinali@ncweb.it>
2020-05-29 11:04:17 +0200

    Creata cartella members e spostato member-list component all'interno. Aggiornati tutti i riferimenti

--- CoreWeb-SPA/src/app/app.module.ts
+++ CoreWeb-SPA/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
