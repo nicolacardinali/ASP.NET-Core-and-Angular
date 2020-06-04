783e1b766da6d99baa5012b2b97df760dfc9057f
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-04 15:44:24 +0200

    import di ngx gallery di kolkov

--- CoreWeb-SPA/src/app/app.module.ts
+++ CoreWeb-SPA/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -52,6 +53,7 @@ export function tokenGetter() {
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
