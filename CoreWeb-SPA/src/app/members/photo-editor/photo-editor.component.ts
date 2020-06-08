7725161fa11e078af1d86f6678d649bac3dfbc84
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-08 16:16:47 +0200

    creato metodo setMainPhoto nel componente che chiama lo userservice e visualizza risultato

--- CoreWeb-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ CoreWeb-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -3,6 +3,8 @@ import { FileUploader } from 'ng2-file-upload';
 import { Photo } from 'src/app/_models/photo';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -16,7 +18,11 @@ export class PhotoEditorComponent implements OnInit {
   hasBaseDropZoneOver: boolean;
   baseUrl = environment.apiUrl;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService
+  ) {
     this.hasBaseDropZoneOver = false;
   }
 
@@ -60,5 +66,16 @@ export class PhotoEditorComponent implements OnInit {
         this.photos.push(photo);
       }
     };
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.userService
+      .setMainPhoto(this.authService.decodedToken.nameid, photo.id)
+      .subscribe(
+        () => {
+          console.log('Successfully set to main');
+        },
+        (error) => this.alertify.error(error)
+      );
   }
 }
