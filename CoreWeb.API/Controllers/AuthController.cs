5a8fe614c4639465da71ad8800a2e732b91d35ec
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-08 16:36:05 +0200

    Dopo login ritorno al client anche l'utente oltre al token

--- CoreWeb.API/Controllers/AuthController.cs
+++ CoreWeb.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using CoreWeb.API.Data;
 using CoreWeb.API.Dtos;
 using CoreWeb.API.Models;
@@ -18,9 +19,11 @@ namespace CoreWeb.API.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        private readonly IMapper _mapper;
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
 
         {
+            this._mapper = mapper;
             this._config = config;
             this._repo = repo;
         }
@@ -78,9 +81,12 @@ namespace CoreWeb.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+
             return Ok(new
             {
-                token = tokenHandler.WriteToken(token)
+                token = tokenHandler.WriteToken(token),
+                user
             });
 
         }
