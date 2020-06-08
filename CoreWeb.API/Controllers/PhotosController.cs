c48569b631ed89b94ba04699a993090f6631e9f6
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-08 12:07:44 +0200

    Inizializzo controller con account cloudinary

--- CoreWeb.API/Controllers/PhotosController.cs
+++ CoreWeb.API/Controllers/PhotosController.cs
@@ -1,6 +1,13 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using CoreWeb.API.Data;
+using CoreWeb.API.Dtos;
 using CoreWeb.API.Helpers;
+using CoreWeb.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -8,17 +15,31 @@ using Microsoft.Extensions.Options;
 namespace CoreWeb.API.Controllers
 {
     [Authorize]
-    [Route("api/users/{userid}/photos")]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
     public class PhotosController : ControllerBase
     {
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
         private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private Cloudinary _cloudinary;
+
         public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
         {
             _cloudinaryConfig = cloudinaryConfig;
             _mapper = mapper;
             _repo = repo;
+
+            // Setup nuovo account cloudinary
+            Account acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc); // Nuova istanza di cloudinary con i dati dell'account
         }
+
+       
     }
 }
