1aa3f514d673d1ce0c07abfdb768462e09a7e75f
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-08 15:20:51 +0200

    Aggiunta configurazione per evitare errore dopo upload foto

--- CoreWeb-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ CoreWeb-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -42,5 +42,9 @@ export class PhotoEditorComponent implements OnInit {
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024,
     });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
   }
 }
