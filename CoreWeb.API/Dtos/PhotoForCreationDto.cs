c07af7b6f9d6b449a766a3975e950b29449ce0a4
Nicola Cardinali <ncardinali@ncweb.it>
2020-06-08 12:19:39 +0200

    aggiunti commenti campi PhotoForCreationDto

--- CoreWeb.API/Dtos/PhotoForCreationDto.cs
+++ CoreWeb.API/Dtos/PhotoForCreationDto.cs
@@ -6,10 +6,10 @@ namespace CoreWeb.API.Dtos
     public class PhotoForCreationDto
     {
         public string Url { get; set; }
-        public IFormFile File { get; set; }
+        public IFormFile File { get; set; } // Photo uploading
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
-        public string PublicID { get; set; }
+        public string PublicID { get; set; } // Ritorna da Cloudinary
         public PhotoForCreationDto()
         {
             DateAdded = DateTime.Now;
